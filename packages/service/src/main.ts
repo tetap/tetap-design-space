444ff291a52203e01c25e413ce48a89ac09cfd63
Tetap <yicai.zheng@idreamsky.com>
2024-08-06 20:14:05 +0800

    update service dependencies

--- packages/service/src/main.ts
+++ packages/service/src/main.ts
@@ -1,4 +1,3 @@
-import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import * as compression from 'compression';
 import { AppModule } from './app.module';
@@ -27,15 +26,6 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new ExceptionsFilter());
   app.useGlobalFilters(new HttpExceptionsFilter());
-
-  // web 安全，防常见漏洞
-  // 注意： 开发环境如果开启 nest static module 需要将 crossOriginResourcePolicy 设置为 false 否则 静态资源 跨域不可访问
-  app.use(
-    helmet({
-      crossOriginOpenerPolicy: { policy: 'same-origin-allow-popups' },
-      crossOriginResourcePolicy: false,
-    }),
-  );
 
   //服务端口
   const port = config.port;
